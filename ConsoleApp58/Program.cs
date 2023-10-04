30d7f20eeb7c8a3402ba734a34e54cfb33f2cde6
Christopher Bellido <pierobellido1901@gmail.com>
2023-10-04 14:42:51 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp58/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp58
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+            double i = 1;
+            double resultado = 0;
+
+            do
+            {
+                resultado = (16 - i) / ( 3 * i ) ;
+                Console.WriteLine("Resultado: " + resultado);
+                i++;
+            } while (i <= n);
+            Console.ReadKey();
+        }
+    }
+}
